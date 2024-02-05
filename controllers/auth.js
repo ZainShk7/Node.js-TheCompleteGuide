dfe4267913ea86fb2663e162b1cb2586bb9c11f3
ZainShk7 <zainshk025@gmail.com>
2024-02-05 20:29:53 +0500

    Login Button Functionality adding

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,15 @@
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login'
-  });
-};
+  // const isLoggedIn =
+  //   req.get("Cookie").split(";")[1].trim().split("=")[1] === "true"
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn
+  })
+}
+
+exports.postLogin = (req, res) => {
+  req.isLoggedIn = true
+  res.setHeader("Set-Cookie", "loggedIn=true HttpOnly")
+  res.redirect("/")
+}
--- controllers/shop.js
+++ controllers/shop.js
@@ -9,7 +9,8 @@ exports.getProducts = (req, res, next) => {
       res.render("shop/product-list", {
         prods: products,
         pageTitle: "All Products",
-        path: "/products"
+        path: "/products",
+        isAuthenticated: req.isLoggedIn
       })
     })
     .catch(err => {
@@ -24,7 +25,8 @@ exports.getProduct = (req, res, next) => {
       res.render("shop/product-detail", {
         product: product,
         pageTitle: product.title,
-        path: "/products"
+        path: "/products",
+        isAuthenticated: req.isLoggedIn
       })
     })
     .catch(err => console.log(err))
@@ -36,7 +38,8 @@ exports.getIndex = (req, res, next) => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
-        path: "/"
+        path: "/",
+        isAuthenticated: req.isLoggedIn
       })
     })
     .catch(err => {
@@ -54,7 +57,8 @@ exports.getCart = (req, res, next) => {
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
-        products: products
+        products: products,
+        isAuthenticated: req.isLoggedIn
       })
     })
     .catch(err => console.log(err))
@@ -83,7 +87,6 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
-  console.log("REQ::: ", req.user)
   User.findById(req.user._id)
     .populate("cart.items.productId")
     .exec()
@@ -114,7 +117,8 @@ exports.getOrders = (req, res, next) => {
       res.render("shop/orders", {
         path: "/orders",
         pageTitle: "Your Orders",
-        orders: orders
+        orders: orders,
+        isAuthenticated: req.isLoggedIn
       })
     })
     .catch(err => console.log(err))
