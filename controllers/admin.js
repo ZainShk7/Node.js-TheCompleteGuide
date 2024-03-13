142c9d82130802a4b91ad014ac39aa21dbba09a8
ZainShk7 <zainshk025@gmail.com>
2024-03-13 21:08:33 +0500

    validation and error handling added at more places

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,13 +1,15 @@
+const { validationResult } = require("express-validator/check")
+
 const Product = require("../models/product")
 
 exports.getAddProduct = (req, res, next) => {
-  if (!req.session.isLoggedIn) {
-    return res.redirect("/login")
-  }
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    editing: false
+    editing: false,
+    hasError: false,
+    errorMessage: null,
+    validationErrors: []
   })
 }
 
@@ -16,6 +18,26 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl
   const price = req.body.price
   const description = req.body.description
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array())
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/edit-product",
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array()
+    })
+  }
+
   const product = new Product({
     title: title,
     price: price,
@@ -50,7 +72,10 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: "Edit Product",
         path: "/admin/edit-product",
         editing: editMode,
-        product: product
+        product: product,
+        hasError: false,
+        errorMessage: null,
+        validationErrors: []
       })
     })
     .catch(err => console.log(err))
@@ -62,6 +87,26 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price
   const updatedImageUrl = req.body.imageUrl
   const updatedDesc = req.body.description
+
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: true,
+      hasError: true,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc,
+        _id: prodId
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array()
+    })
+  }
 
   Product.findById(prodId)
     .then(product => {
@@ -82,7 +127,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find({ userId: req.user._id })
+    // .select('title price -_id')
+    // .populate('userId', 'name')
     .then(products => {
+      console.log(products)
       res.render("admin/products", {
         prods: products,
         pageTitle: "Admin Products",
--- routes/auth.js
+++ routes/auth.js
@@ -10,37 +10,56 @@ router.get("/login", authController.getLogin)
 
 router.get("/signup", authController.getSignup)
 
-router.post("/login", authController.postLogin)
+router.post(
+  "/login",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email address.")
+      .normalizeEmail(),
+    body("password", "Password has to be valid.")
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim()
+  ],
+  authController.postLogin
+)
 
 router.post(
   "/signup",
   [
     check("email")
       .isEmail()
-      .withMessage("please enter a valid email.")
+      .withMessage("Please enter a valid email.")
       .custom((value, { req }) => {
-        // if (value === "sampleemail@test.com") {
-        //   throw new Error("This email address if forbidden.")
+        // if (value === 'test@test.com') {
+        //   throw new Error('This email address if forbidden.');
         // }
-        // return true
+        // return true;
         return User.findOne({ email: value }).then(userDoc => {
           if (userDoc) {
-            return Promise.reject("E-mail already exist, Try new one")
+            return Promise.reject(
+              "E-Mail exists already, please pick a different one."
+            )
           }
         })
-      }),
+      })
+      .normalizeEmail(),
     body(
       "password",
-      "please enter password that contains no symbol and sould be 5 characters atleast."
+      "Please enter a password with only numbers and text and at least 5 characters."
     )
       .isLength({ min: 5 })
-      .isAlphanumeric(),
-    body("confirmPassword").custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error("Passwords not Matched!")
-      }
-      return true
-    })
+      .isAlphanumeric()
+      .trim(),
+    body("confirmPassword")
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Passwords have to match!")
+        }
+        return true
+      })
   ],
   authController.postSignup
 )
@@ -49,8 +68,10 @@ router.post("/logout", authController.postLogout)
 
 router.get("/reset", authController.getReset)
 
+router.post("/reset", authController.postReset)
+
 router.get("/reset/:token", authController.getNewPassword)
 
-router.post("/new-passsword", authController.postNewPassword)
+router.post("/new-password", authController.postNewPassword)
 
 module.exports = router
