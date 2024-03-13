90f66dae1b18c29d2710da5ae84efaa389c3a0e7
ZainShk7 <zainshk025@gmail.com>
2024-03-13 20:42:25 +0500

    login form validation added with errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,7 +16,12 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: "",
+      password: ""
+    },
+    validationErrors: []
   })
 }
 
@@ -42,15 +47,28 @@ exports.postLogin = (req, res) => {
     return res.status(422).render("auth/login", {
       path: "/login",
       pageTitle: "Login",
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password
+      },
+      validationErrors: errors.array()
     })
   }
 
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
-        req.flash("error", "Invalid email/password")
-        return res.redirect("/login")
+        return res.status(422).render("auth/login", {
+          path: "/login",
+          pageTitle: "Login",
+          errorMessage: "Invalid email or password.",
+          oldInput: {
+            email: email,
+            password: password
+          },
+          validationErrors: []
+        })
       }
       bcrypt
         .compare(password, user.password)
@@ -63,8 +81,16 @@ exports.postLogin = (req, res) => {
               res.redirect("/")
             })
           }
-          req.flash("error", "Invalid email/password")
-          return res.redirect("/login")
+          return res.status(422).render("auth/login", {
+            path: "/login",
+            pageTitle: "Login",
+            errorMessage: "Invalid email or password.",
+            oldInput: {
+              email: email,
+              password: password
+            },
+            validationErrors: []
+          })
         })
         .catch(err => {
           console.log(err)
